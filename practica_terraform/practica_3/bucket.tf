ea7caf2c73adb7bfee28b9ef7fcd00150bc5d109
Miguel Alberto Plazas Wadynski <ambigus9@gmail.com>
2024-03-05 18:15:01 -0500

    Agregando Practica 3

---
+++ practica_terraform/practica_3/bucket.tf
@@ -0,0 +1,20 @@
+resource "aws_s3_bucket" "proveedores" {
+
+  count  = 6
+  bucket = "tf-id-${random_string.sufijo[count.index].id}"
+
+  tags = {
+    Owner       = "Bucket"
+    Environment = "Dev"
+    Office      = "proveedores"
+  }
+
+}
+
+resource "random_string" "sufijo" {
+  count   = 6
+  length  = 8
+  special = false
+  upper   = false
+  numeric = false
+}
