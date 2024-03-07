5a6fdb2989a6c73effe72e87fac2e0634bdb543d
Miguel Alberto Plazas Wadynski <ambigus9@gmail.com>
2024-03-06 20:36:29 -0500

    Agregando Practica 5 - Variables de Terrafom

---
+++ practica_terraform/practica_5/providers.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.36.0"
+    }
+  }
+  required_version = "~>1.7.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+  alias = "virginia"
+}
+
+provider "aws" {
+  region = "us-east-2"
+  alias = "ohio"
+}
