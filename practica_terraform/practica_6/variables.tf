679f85a16f7f7922c7056110cf171af4ba8b601a
Miguel Alberto Plazas <ambigus9@gmail.com>
2024-03-30 13:58:43 -0500

    Agregando Variables de Tipo Lista y Map

--- practica_terraform/practica_6/variables.tf
+++ practica_terraform/practica_6/variables.tf
@@ -3,12 +3,22 @@ variable "virginia_cidr" {
     type = string
 }
 
-variable "public_subnet" {
-    description = "CIDR public subnet"
-    type = string
+# variable "public_subnet" {
+#     description = "CIDR public subnet"
+#     type = string
+# }
+
+# variable "private_subnet" {
+#     description = "CIDR private subnet"
+#     type = string
+# }
+
+variable "subnets" {
+    description = "Lista de subnets"
+    type = list(string)
 }
 
-variable "private_subnet" {
-    description = "CIDR private subnet"
-    type = string
+variable "tags" {
+    description = "Tags del Proyecto"
+    type = map(string)
 }
