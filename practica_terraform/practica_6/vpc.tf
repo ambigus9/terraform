22865d283481b992958bfc3c4a94a2b81fac59ec
Miguel Alberto Plazas <ambigus9@gmail.com>
2024-03-30 13:59:07 -0500

    Actualizando Tags Name para la VPC

--- practica_terraform/practica_6/vpc.tf
+++ practica_terraform/practica_6/vpc.tf
@@ -1,18 +1,23 @@
 resource "aws_vpc" "vpc_virginia" {
   cidr_block = var.virginia_cidr
   tags = {
-    Name = "VPC VIRGINIA v10"
-    env  = "Dev"
+    Name = "VPC VIRGINIA v11"
   }
 }
 
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.vpc_virginia.id
-  cidr_block              = var.public_subnet
+  cidr_block              = var.subnets[0]
   map_public_ip_on_launch = true
+  tags = {
+    Name = "Subnet Pública"
+  }
 }
 
 resource "aws_subnet" "private_subnet" {
   vpc_id     = aws_vpc.vpc_virginia.id
-  cidr_block = var.private_subnet
+  cidr_block = var.subnets[1]
+  tags = {
+    Name = "Subnet Privada"
+  }
 }
