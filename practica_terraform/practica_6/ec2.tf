b417b1d1110ea7fdb58407a320ea1e950bde7940
Miguel Alberto Plazas <ambigus9@gmail.com>
2024-04-10 08:46:43 -0500

    Renombre menor

--- practica_terraform/practica_6/ec2.tf
+++ practica_terraform/practica_6/ec2.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "public_instance" {
   ami                     = "ami-0c101f26f147fa7fd" # us-east-1
   instance_type           = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
+  key_name = data.aws_key_pair.key.key_name
   tags = {
-    "Name": "Instancia Gratis"
+    "Name": "instancia_terraform"
   }
 }
