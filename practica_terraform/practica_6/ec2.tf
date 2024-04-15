5666172bc25c87f44e414a1271b6f105a41ca6ec
Miguel Alberto Plazas <ambigus9@gmail.com>
2024-04-15 12:39:45 -0500

    Agregando Lifecycle a la instancia

--- practica_terraform/practica_6/ec2.tf
+++ practica_terraform/practica_6/ec2.tf
@@ -4,7 +4,10 @@ resource "aws_instance" "public_instance" {
   instance_type           = "t2.micro"
   subnet_id = aws_subnet.public_subnet.id
   key_name = data.aws_key_pair.key.key_name
-  tags = {
-    "Name": "instancia_terraform"
+  
+  lifecycle {
+    create_before_destroy = true # crea el recurso nuevo primero y luego destruye el actual ante cualquier cambio
+    prevent_destroy = true # evita que se destruya el recurso
   }
+
 }
