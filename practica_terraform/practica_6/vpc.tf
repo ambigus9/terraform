39433ce3d8df83f31e872d5834788d3872503b68
Miguel Alberto Plazas <ambigus9@gmail.com>
2024-04-15 12:40:02 -0500

    Actualización menor

--- practica_terraform/practica_6/vpc.tf
+++ practica_terraform/practica_6/vpc.tf
@@ -1,7 +1,7 @@
 resource "aws_vpc" "vpc_virginia" {
   cidr_block = var.virginia_cidr
   tags = {
-    Name = "VPC VIRGINIA v11"
+    Name = "VPC VIRGINIA v12"
   }
 }
 
@@ -20,4 +20,4 @@ resource "aws_subnet" "private_subnet" {
   tags = {
     Name = "Subnet Privada"
   }
-}
+}  
