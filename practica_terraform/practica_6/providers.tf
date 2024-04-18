ffe6cfe11e3a59ac3247c811dc726f023d1d735e
Miguel Alberto Plazas Wadynski <ambigus9@gmail.com>
2024-04-18 16:27:31 -0500

    Actualizando default tags

--- practica_terraform/practica_6/providers.tf
+++ practica_terraform/practica_6/providers.tf
@@ -5,13 +5,21 @@ terraform {
       version = "5.36.0"
     }
   }
-  required_version = "~>1.7.0"
+  required_version = "~>1.8.0"
 }
 
 provider "aws" {
   region = "us-east-1"
   alias = "virginia"
+  
   default_tags {
-    tags = var.tags
+    tags = {
+        "env": "develop"
+        "ownder": "Miguel Alberto Plazas Wadynski"
+        "cloud": "AWS"
+        "IAC": "Terraform"
+        "IAC_Version": "1.7.4"
+    }
   }
+
 }
