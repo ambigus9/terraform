56901a35a04e47979dc22a0ed9d2e5ab8c36929c
Miguel Alberto Plazas Wadynski <ambigus9@gmail.com>
2024-04-18 16:26:43 -0500

    Agregando Security Group, Internet Gateway y Route Table

--- practica_terraform/practica_6/vpc.tf
+++ practica_terraform/practica_6/vpc.tf
@@ -20,4 +20,65 @@ resource "aws_subnet" "private_subnet" {
   tags = {
     Name = "Subnet Privada"
   }
+  depends_on = [ aws_instance.public_instance ]
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_virginia.id
+
+  tags = {
+    Name = "igw vpc virginia"
+  }
+}
+
+## Route Table (Custom)
+resource "aws_route_table" "crt" {
+  vpc_id = aws_vpc.vpc_virginia.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public custom route table (crt)"
+  }
+}
+
+## Route Table Association (Custom)
+resource "aws_route_table_association" "crta_public_subnet" {
+  subnet_id      = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.crt.id
+}
+
+
+# Adding - Segurity Group for enable connecting to EC2 Public Instance
+resource "aws_security_group" "sg_public_instance" {
+  name        = "Public Instance SG"
+  description = "Allo SSH inbound traffic and ALL egress traffic"
+  vpc_id      = aws_vpc.vpc_virginia.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  description = "SSH over Internet"
+  security_group_id = aws_security_group.sg_public_instance.id
+  cidr_ipv4         = var.sg_ingress_cidr
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+
+  tags = {
+    Name = "Public Instance SG"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.sg_public_instance.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
 }
