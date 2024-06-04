2d1ea1b4aa95484084a4864225395b06d51c2508
ambigus9 <ambigus9@gmail.com>
2024-06-04 10:54:06 -0500

    Updating versioning on S3 bucket

--- practica_terraform/practica_7/s3.tf
+++ practica_terraform/practica_7/s3.tf
@@ -1,4 +1,13 @@
 
 resource "aws_s3_bucket" "cerberus" {
+
   bucket = local.s3-sufix
+
+}
+
+resource "aws_s3_bucket_versioning" "versioning_cerberus" {
+  bucket = aws_s3_bucket.cerberus.id
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
